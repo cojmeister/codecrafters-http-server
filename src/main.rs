dc70503f71ec19836076581ef2f88dc5846e4ad2
cojmeister <luqas.c@gmail.com>
2024-04-14 20:43:20 +0300

    Third stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
 // Uncomment this block to pass the first stage
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::TcpListener,
+};
+
+const CRLF: &str = "\r\n";
+
+const OK_RESPONSE: String = format!("HTTP/1.1 200 OK{}{}", CRLF, CRLF);
+
+const NOT_FOUND_RESPONSE: String = format!("HTTP/1.1 404 Not Found{}{}", CRLF, CRLF);
+
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq)]
+enum HttpMethod {
+    GET,
+    POST,
+}
+
+impl FromStr for HttpMethod {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<HttpMethod, Self::Err> {
+        match input {
+            "GET" => Ok(HttpMethod::GET),
+            "POST" => Ok(HttpMethod::POST),
+            _ => Err(()),
+        }
+    }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,13 +41,31 @@ fn main() {
             Ok(mut _stream) => {
                 println!("accepted new connection");
 
-                let response = "HTTP/1.1 200 OK\r\n\r\n";
-
-                _stream.write_all(response.as_bytes()).unwrap();
+                handle_connection(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: std::net::TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let mut parts = http_request[0].split_whitespace();
+
+    let _method: HttpMethod = HttpMethod::from_str(parts.next().unwrap()).unwrap();
+
+    let response = match parts.next().unwrap() {
+        "/" => OK_RESPONSE,
+        _ => NOT_FOUND_RESPONSE,
+    };
+
+    stream.write(response.as_bytes());
 }
