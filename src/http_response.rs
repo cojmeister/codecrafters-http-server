97aa19c6a4f60e6c94c05cf709df735dd74fe3f7
cojmeister <luqas.c@gmail.com>
2024-04-26 22:00:00 +0300

    Made http response

--- src/http_response.rs
+++ src/http_response.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, Debug)]
 pub enum ContentType {
     TextPlain,
     ApplicationOctetStream,
@@ -53,11 +53,11 @@ impl Display for HttpResponse {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut base = format!("HTTP/1.1 {} {}", self.code, self.message);
         if self.content_type != ContentType::None {
-            base += format!("\r\nContent-Type: {}", self.content_type);
+            base += (&format!("\r\nContent-Type: {:?}", self.content_type));
         }
         if self.content_length > 0 {
-            base += format!("\r\nContent-Length: {}", self.content_length);
-            base += format!("\r\n\r\n{}", self.content);
+            base += (&format!("\r\nContent-Length: {}", self.content_length));
+            base += (&format!("\r\n\r\n{}", self.content));
         }
 
         return write!(f, "{}", base)
